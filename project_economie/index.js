cdf3c6eacca077425a0f357e55c23c1d8b00b752
theCYBERbro <56652297+theCYBERbro@users.noreply.github.com>
2021-11-29 15:57:38 +0200

    ✅economie

--- project_economie/index.js
+++ project_economie/index.js
@@ -12,16 +12,19 @@ void tabel()
     for(int i=1;i<=randuri;++i)
         cin>>umgy[i];
         int ix=1,iy=1;
+
         while(ix<=randuri && iy<=randuri && s>0)
         {
             float efx=umgx[ix]/px,efy=umgy[iy]/py;
             if(efx==efy)
-                cout<<"x"<<ix<<" + y"<<iy<<'\n',s-=px+py,ix++,iy++;
+                cout<<"x"<<ix<<" + y"<<iy<<'\n';
+                s-=px+py,ix++,iy++;
+                else if(efx>efy)
+                    cout<<"x"<<ix<<'\n';
+                    s-=px,ix++;
                 else
-                    if(efx>efy)
-                    cout<<"x"<<ix<<'\n',s-=px,ix++;
-                else
-                    cout<<"y"<<iy<<'\n',s-=py,iy++;
+                    cout<<"y"<<iy<<'\n';
+                    s-=py,iy++;
         }
 
 
@@ -39,74 +42,87 @@ int main()
 }
 */
 
-function tabel() {
+function main() {
   document.getElementById("submit").onclick = function () {
-    // var rows = document.getElementById("nr_rand").value;
-    // var columns = 2;
-    // // if(rows == null )
-    // // rows=10
-    // var output =
-    //   '<table border="1" cellspacing="0" cellpadding="5" class="table">';
+    //in debugging
+    var res = "";
+    var ix = 1,
+      iy = 1;
+    var px = document.getElementById("px").value;
+    var py = document.getElementById("py").value;
+    var suma = document.getElementById("sum").value;
+    var nr_rand = document.getElementById("nr_rand").value;
+    var umgx = document.getElementById("umgx").value.split(" ");
+    var umgy = document.getElementById("umgy").value.split(" ");
 
-    // function createTable(nr_rand, columns) {
-    //   for (var i = 1; i <= rows; i++) {
-    //     output = output + "<tr>";
-    //     for (var j = 1; j <= columns; j++) {
-    //       output =
-    //         output +
-    //         "<td>" +
-    //         '<input class="input" type="text" placeholder="introduceti pentru">' +
-    //         "</td>";
-    //     }
-    //     output = output + "</tr>";
-    //   }
-    //   output = output + "</table>";
-    //   document.getElementById("container").innerHTML = output;
-    // }
-    // createTable(rows, columns);
+    //afisare
 
-    citire();
-    afisare(); //buggy
+    var result =
+      "pret x =" +
+      px +
+      "<br>" +
+      "pret y =" +
+      py +
+      "<br>" +
+      "Suma = " +
+      suma +
+      "<br>" +
+      "nr randuri = " +
+      nr_rand +
+      "<br>" +
+      "UMGX =" +
+      umgx +
+      "<br>" +
+      "UMGY =" +
+      umgy;
+    document.getElementById("container").innerHTML = result;
+
+    //algo
+    // console.log("algo start:\n");
+
+    //console log good
+
+    while (ix <= nr_rand+1 && iy <= nr_rand+1 && suma > 0) {
+      var efx = umgx[ix-1] / px,
+        efy = umgy[iy-1] / py;
+      // console.log(
+      //   "var: " +
+      //     "efx: " +
+      //     efx +
+      //     " " +
+      //     "efy :" +
+      //     efy +
+      //     " " +
+      //     umgx[ix-1] +
+      //     " " +
+      //     umgy[iy-1] +
+      //     "\n"
+      // );
+      // console.log("var:" + umgx[ix] + "\n");
+
+      if (efx == efy) {
+        // console.log("1");
+
+        res += "x" + ix + " + y" + iy + "<br>";
+        // suma -= px + py, ix++, iy++;
+        (suma -= px), (suma -= py), ix++, iy++;
+      } else if (efx > efy) {
+        // console.log("2");
+
+        res += "x" + ix + "<br>";
+        (suma -= px), ix++;
+      } else {
+        // console.log("3");
+
+        res += "y" + iy + "<br>";
+        (suma -= py), iy++;
+      }
+    }
+    console.log("output generated: " + res);
+
+    document.getElementById("result").innerHTML =
+      res + "in total " + (ix - 1) + " de x si " + (iy - 1) + " de y";
   };
-}
-
-function citire() {
-  var px = document.getElementById("px").value;
-  var py = document.getElementById("py").value;
-  var suma = document.getElementById("sum").value;
-  var nr_rand = document.getElementById("px").value;
-  var u_x = 0,
-    u_y = 0;
-  var umgx = Array();
-  var umgy = Array();
-  umgx[u_x] = document.getElementById("umgx").value;
-  umgy[u_y] = document.getElementById("umgy").value;
-  //  alert("Element: " + array[x] + " Added at index " + x);
-  u_x++;
-  u_y++;
-  //   document.getElementById("text1").value = "";
-}
-
-function afisare() {
-  var result =
-    "pret x =" +
-    px.value +
-    "<br>" +
-    "pret y =" +
-    py.value +
-    "<br>" +
-    "Suma = " +
-    sum.value +
-    "<br>" +
-    "nr randuri = " +
-    nr_rand.value +
-    "<br>" +
-    "UMGX =" +
-    umgx.value +
-    "<br>" +
-    "UMGY =" +
-    umgy.value;
-  document.getElementById("container").innerHTML = result;
 }
 
 function add_element_to_array() {
