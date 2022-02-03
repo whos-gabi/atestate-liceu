9214093302da11aca4d142b2d37d7cf14438a605
theCYBERbro <56652297+theCYBERbro@users.noreply.github.com>
2022-02-04 00:31:39 +0200

    update 6 & 8

--- project6/assets/js/bootstrap.js
+++ project6/assets/js/bootstrap.js
@@ -1,8 +1,4 @@
-/*!
-  * Bootstrap v5.0.2 (https://getbootstrap.com/)
-  * Copyright 2011-2021 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
-  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
-  */
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@popperjs/core')) :
   typeof define === 'function' && define.amd ? define(['@popperjs/core'], factory) :
@@ -30,13 +26,6 @@
   }
 
   var Popper__namespace = /*#__PURE__*/_interopNamespace(Popper);
-
-  /**
-   * --------------------------------------------------------------------------
-   * Bootstrap (v5.0.2): dom/selector-engine.js
-   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
-   * --------------------------------------------------------------------------
-   */
 
   /**
    * ------------------------------------------------------------------------
@@ -101,13 +90,6 @@
     }
 
   };
-
-  /**
-   * --------------------------------------------------------------------------
-   * Bootstrap (v5.0.2): util/index.js
-   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
-   * --------------------------------------------------------------------------
-   */
 
   const MAX_UID = 1000000;
   const MILLISECONDS_MULTIPLIER = 1000;
